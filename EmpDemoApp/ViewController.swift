6a13e02cae6d07a87459d4fa4b55cb3cbe19dd86
Nitesh Isave <nitesh.isave@infinx.com>
2022-04-29 11:10:15 +0530

    Initial Commit

---
+++ EmpDemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmpDemoApp
+//
+//  Created by Nitesh on 29/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
